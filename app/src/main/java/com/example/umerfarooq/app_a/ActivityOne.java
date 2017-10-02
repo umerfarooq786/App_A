5f746a0303d0b89e4fe5df76928762d016d4ec0a
Umer Farooq <umar.farooq78686@gmail.com>
2017-10-02 13:06:56 -0600

    Initial commit

---
+++ app/src/main/java/com/example/umerfarooq/app_a/ActivityOne.java
@@ -0,0 +1,13 @@
+package com.example.umerfarooq.app_a;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_one);
+    }
+}
